9eafe47d1b2b5c2db8115b274f5c2bdfcebc6f17
Katherin26 <ft.katheochoa@gmail.com>
2022-05-19 22:14:08 -0400

    added constants

--- 01.nft-marketplace/screens/Home.js
+++ 01.nft-marketplace/screens/Home.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Text } from "react-native";
+import { useState } from "react";
+import { View, SafeArea, FlatList, Text } from "react-native";
+//NOTE: FlatList = That lowers the memory usage therefore it technically.
+//Uses lazy loading for better optimization.
 
 function Home() {
   return <Text>Homeeeeeeeeeee</Text>;
