e37c438430cdf3e5d5ed95aea2b0e27127d9facf
Katherin26 <ft.katheochoa@gmail.com>
2022-06-01 19:53:28 -0400

    added details component

--- 01.nft-marketplace/screens/Details.js
+++ 01.nft-marketplace/screens/Details.js
@@ -1,8 +1,40 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../constants";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusedStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
 
-function Details() {
-  return <Text>Details</Text>;
+/*NOTE: route and navigation = we get that by default because we are using the details
+as a component inside of our stack screen 
+*/
+function Details({ route, navigation }) {
+  const { data } = route.params;
+
+  return (
+    <SafeAreaView>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        transLucent={true}
+      />
+      <View>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default Details;
