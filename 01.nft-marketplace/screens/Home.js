88d1911977548618b9deafc08688c3781276c3f0
Katherin26 <ft.katheochoa@gmail.com>
2022-06-02 17:23:17 -0400

    added HOme component

--- 01.nft-marketplace/screens/Home.js
+++ 01.nft-marketplace/screens/Home.js
@@ -1,28 +1,46 @@
 import React from "react";
 import { useState } from "react";
-import { View, SafeAreaView, FlatList, Text } from "react-native";
+import { View, SafeAreaView, FlatList } from "react-native";
+
 //NOTE: FlatList = That lowers the memory usage therefore it technically.
 //Uses lazy loading for better optimization.
 
 import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
+/*NOTE: HandleChange */
+
 function Home() {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length) {
+      setNftData(filteredData);
+    } else {
+      setNftData(NFTData);
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar
         backgroundColor={COLORS.primary}
         barStyle={"dark-content"}
       />
-      <Text>Text</Text>
+
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
         <View
